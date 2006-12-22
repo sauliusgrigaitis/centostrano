230f2fe3ad70097dc6c5928e22e732c4fa413089
mbailey <mbailey@479ab18a-db23-0410-b897-fa3724b950b0>
2006-12-22 09:06:47 +0000

    added new functions: useradd(), groupadd(), add_user_to_group() moved functions from recipes.rb: download_src(), unpack_src(), install_from_src()
    git-svn-id: svn://scm.deprecated.org/deprec/trunk@7 479ab18a-db23-0410-b897-fa3724b950b0

--- lib/deprec/capistrano_extensions/deprec_extensions.rb
+++ lib/deprec/capistrano_extensions/deprec_extensions.rb
@@ -25,6 +25,64 @@ module Deprec
       echo '#{value}' >> #{filename}
     END
   end
+  
+  # create new user account on target system
+  def useradd(user)
+    puts run_method
+    send(run_method, "grep '^#{user}:' /etc/passwd || /usr/sbin/useradd -m #{user}")
+  end
+  
+  # create a new group on target system
+  def groupadd(group)
+    # XXX I don't like specifying the path to groupadd - need to sort out paths before long
+    send(run_method, "grep '#{group}:' /etc/group || sudo /usr/sbin/groupadd #{group}")
+  end
+  
+  # add group to the list of groups this user belongs to
+  def add_user_to_group(user, group)
+    send(run_method, "groups #{user} | grep ' #{group} ' || sudo /usr/sbin/usermod -G #{group} -a #{user}")
+  end
+  
+  # download source package if we don't already have it
+  def download_src(src_package, src_dir)
+    deprec.groupadd(group)
+    sudo "test -d #{src_dir} || sudo mkdir #{src_dir}" 
+    sudo "chgrp -R #{group} #{src_dir}"
+    sudo "chmod -R g+w #{src_dir}"
+    # XXX check if file exists and if we have and MD5 hash or bytecount to compare against
+    # XXX if so, compare and decide if we need to download again
+    sudo "sh -c 'cd #{src_dir} && test -f #{src_package[:file]} || wget #{src_package[:url]}'"
+  end
+  
+  # unpack src and make it writable by the group
+  def unpack_src(src_package, src_dir)
+    package_dir = File.join(src_dir, src_package[:dir])
+    sudo <<-SUDO
+    sh -c '
+      cd #{src_dir};
+      test -d #{package_dir}.old && rm -fr #{package_dir}.old;
+      test -d #{package_dir} && mv #{package_dir} #{package_dir}.old;
+      #{src_package[:unpack]}
+      chgrp -R #{group} #{package_dir};  
+      chmod -R g+w #{package_dir};
+    '
+    SUDO
+  end
+  
+  # install package from source
+  def install_from_src(src_package, src_dir)
+    package_dir = File.join(src_dir, src_package[:dir])
+    unpack_src(src_package, src_dir)
+    sudo <<-SUDO
+    sh -c '
+      cd #{package_dir};
+      #{src_package[:configure]}
+      #{src_package[:make]}
+      #{src_package[:install]}
+      #{src_package[:post_install]}
+      '
+    SUDO
+  end
 
 end
 
