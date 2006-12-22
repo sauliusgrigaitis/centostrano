90872e58f43f45c2b6f29c79fb857890609a86c6
mbailey <mbailey@479ab18a-db23-0410-b897-fa3724b950b0>
2006-12-22 09:40:07 +0000

    added install_vmware_server() and install_vmware_mui()
    git-svn-id: svn://scm.deprecated.org/deprec/trunk@9 479ab18a-db23-0410-b897-fa3724b950b0

--- lib/deprec/recipes/vmware.rb
+++ lib/deprec/recipes/vmware.rb
@@ -2,6 +2,42 @@ Capistrano.configuration(:must_exist).load do
   set :vm_dir, '/var/vm'
   set :stemserver, 'stemserver_ubuntu_6.06.1'
   
+  # currently only for CentOS
+  task :install_vmware_server do
+    version = 'VMware-server-1.0.1-29996.i386'
+    set :src_package, {
+      :file => version + '.rpm',
+      :url => "http://10.0.100.45/download/vmware/#{version}.rpm",
+      :install => "rpm -i #{File.join(src_dir, version + '.rpm')};"
+    }
+    deprec.download_src(src_package, src_dir)
+    sudo src_package[:install]
+    sudo "yum install gcc" # when you select the develpment packages on CentOS it doesn't give you gcc!
+    # XXX work out how to do this interactive through capistrano
+    puts
+    puts "IMPORTANT"
+    puts "sudo /usr/bin/vmware-config.pl"
+    puts
+  end
+  
+  task :install_vmware_mui do
+    version = 'VMware-mui-1.0.1-29996'
+    src_package = {
+      :file => version + '.tar.gz',
+      :dir => 'vmware-mui-distrib',  
+      :url => "http://10.0.100.45/download/vmware/#{version}.tar.gz",
+      :unpack => "tar zxf #{version}.tar.gz;",
+      :install => './vmware-install.pl;'
+    }
+    deprec.download_src(src_package, src_dir)
+    deprec.unpack_src(src_package, src_dir)
+    # XXX work out how to do this interactive through capistrano
+    puts
+    puts "IMPORTANT - you need to log in and run"
+    puts "cd /usr/local/src/vmware-mui-distrib && sudo ./vmware-install.pl"
+    puts
+  end
+  
   task :replicate_stemserver do
     sudo <<-SUDO
     sh -c '
