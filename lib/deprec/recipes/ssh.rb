162065780bdda54e8b2497304140a0f44efdfca8
mbailey <mbailey@479ab18a-db23-0410-b897-fa3724b950b0>
2007-05-23 20:02:17 +0000

    bugfix: setup_ssh_keys() - print message and exit if ssh_options[:keys] not set
    git-svn-id: svn://scm.deprecated.org/deprec/trunk@74 479ab18a-db23-0410-b897-fa3724b950b0

--- lib/deprec/recipes/ssh.rb
+++ lib/deprec/recipes/ssh.rb
@@ -3,6 +3,19 @@ Capistrano.configuration(:must_exist).load do
   # set :user, (defined?(user) ? user : ENV['USER'])
   desc "Copies contents of ssh public keys into authorized_keys file"
   task :setup_ssh_keys do
+    
+    unless ssh_options[:keys]  
+      puts <<-ERROR
+
+      You need to define the name of your SSH key(s)
+      e.g. ssh_options[:keys] = %w(/Users/someuser/.ssh/id_dsa)
+
+      You can put this in your .caprc file in your home directory.
+
+      ERROR
+      exit
+    end
+    
     sudo "test -d ~/.ssh || mkdir ~/.ssh"
     sudo "chmod 0700 ~/.ssh"    
     put(ssh_options[:keys].collect{|key| File.read(key+'.pub')}.join("\n"),
