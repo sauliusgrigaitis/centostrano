c8485acfa928aaf274be74e3caead67f825ea2a3
mbailey <mbailey@479ab18a-db23-0410-b897-fa3724b950b0>
2007-10-14 04:06:37 +0000

    this patch has been integrated with capistrano
    git-svn-id: svn://scm.deprecated.org/deprec/trunk@133 479ab18a-db23-0410-b897-fa3724b950b0

--- lib/deprec/recipes/deprec.rb
+++ lib/deprec/recipes/deprec.rb
@@ -7,53 +7,6 @@ Capistrano::Configuration.instance(:must_exist).load do
       set(name, *args, &block)
     end
   end
-  
-  # monkey patch Capistrano to specify a sudo prompt in order to disambiguate
-  # it's password prompts on different OS's
-  module Capistrano
-    class Configuration
-      module Actions
-        module Invocation
-          def sudo(command, options={}, &block)
-            block ||= self.class.default_io_proc
-
-            options = options.dup
-            as = options.delete(:as)
-
-            prompt_text = fetch(:sudo_prompt, "sudo password: ") 
-            prompt = prompt_text && "-p '#{prompt_text}'"
-
-            user = as && "-u #{as}"
-            command = [fetch(:sudo, "sudo"), prompt, user, command].compact.join(" ")
-
-            run(command, options, &sudo_behavior_callback(block))
-          end
-          def sudo_behavior_callback(fallback) #:nodoc:
-            # in order to prevent _each host_ from prompting when the password
-            # was wrong, let's track which host prompted first and only allow
-            # subsequent prompts from that host.
-            prompt_host = nil
-
-            prompt_text = fetch(:sudo_prompt, "sudo password: ") 
-
-            Proc.new do |ch, stream, out|
-              if out =~ /^#{prompt_text}/
-                ch.send_data "#{self[:password]}\n"
-              elsif out =~ /try again/
-                if prompt_host.nil? || prompt_host == ch[:server]
-                  prompt_host = ch[:server]
-                  logger.important out, "#{stream} :: #{ch[:server]}"
-                  reset! :password
-                end
-              else
-                fallback.call(ch, stream, out)
-              end
-            end
-          end
-        end
-      end
-    end
-  end
     
   CHOICES_WEBSERVER = [:nginx, :apache, :none]
   CHOICES_APPSERVER = [:mongrel, :webrick, :none]
