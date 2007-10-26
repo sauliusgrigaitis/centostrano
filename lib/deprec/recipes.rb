896fa950328fdeca000c1873fffb68a62497daca
mbailey <mbailey@479ab18a-db23-0410-b897-fa3724b950b0>
2007-10-26 04:21:56 +0000

    hmmm...not sure this is that clever
    git-svn-id: svn://scm.deprecated.org/deprec/trunk@136 479ab18a-db23-0410-b897-fa3724b950b0

--- lib/deprec/recipes.rb
+++ lib/deprec/recipes.rb
@@ -1,16 +1,25 @@
-path = "/Users/mbailey/work/deprec/lib/"
+unless Capistrano::Configuration.respond_to?(:instance)
+  abort "capistrano/ext/multistage requires Capistrano 2"
+end
+load "#{File.dirname(__FILE__)}/vmbuilder_plugins/all.rb"
+load "#{File.dirname(__FILE__)}/recipes/deprec.rb"
+load "#{File.dirname(__FILE__)}/capistrano_extensions/deprec2_extensions.rb"
+load "#{File.dirname(__FILE__)}/third_party/vmbuilder/plugins/std.rb"
+
+Dir["#{File.dirname(__FILE__)}/recipes/*.rb"].each { |ext| load ext }
+
+
+# path = "/Users/mbailey/work/deprec/lib/"
 # path = ''
-require "#{path}deprec/recipes/deprec"
-require "#{path}deprec/recipes/users"
-require "#{path}deprec/recipes/deprecated"
-require "#{path}deprec/recipes/canonical"
-require "#{path}deprec/recipes/ssh"
-require "#{path}deprec/recipes/apache"
-require "#{path}deprec/recipes/nginx"
-require "#{path}deprec/recipes/mongrel"
-require "#{path}deprec/recipes/mysql"
-require "#{path}deprec/recipes/php"
+# require "#{path}deprec/recipes/users"
+# require "#{path}deprec/recipes/deprecated"
+# require "#{path}deprec/recipes/canonical"
+# require "#{path}deprec/recipes/ssh"
+# require "#{path}deprec/recipes/apache"
+# require "#{path}deprec/recipes/nginx"
+# require "#{path}deprec/recipes/mongrel"
+# require "#{path}deprec/recipes/mysql"
+# require "#{path}deprec/recipes/php"
 # require "#{path}deprec/recipes/subversion.rb"
 # require "#{path}deprec/recipes/trac.rb"
-require "#{path}deprec/third_party/vmbuilder/plugins"
-require "#{path}deprec/capistrano_extensions/deprec_extensions"
+# require "#{path}deprec/third_party/vmbuilder/plugins"
