bd61ee14f69ebfa1751d4f6d2c3725594f73cc00
mbailey <mbailey@479ab18a-db23-0410-b897-fa3724b950b0>
2007-10-28 07:14:02 +0000

    rubygems-0.9.4
    git-svn-id: svn://scm.deprecated.org/deprec/trunk@145 479ab18a-db23-0410-b897-fa3724b950b0

--- lib/deprec/recipes/ruby.rb
+++ lib/deprec/recipes/ruby.rb
@@ -1,15 +1,19 @@
-task :install_rubygems do
-  version = 'rubygems-0.9.2'
-  set :src_package, {
-    :file => version + '.tgz',
-    :md5sum => 'cc525053dd465ab6e33af382166fa808  rubygems-0.9.2.tgz',
-    :dir => version,
-    :url => "http://rubyforge.org/frs/download.php/17190/#{version}.tgz",
-    :unpack => "tar zxf #{version}.tgz;",
-    :install => '/usr/bin/ruby1.8 setup.rb;'
-  }
-  deprec2.download_src(src_package, src_dir)
-  deprec2.install_from_src(src_package, src_dir)
-  gem2.upgrade
-  gem2.update_system
+Capistrano::Configuration.instance(:must_exist).load do 
+
+  task :install_rubygems do
+    version = 'rubygems-0.9.4'
+    set :src_package, {
+      :file => version + '.tgz',
+      :md5sum => 'b5680acaa019c80ea44fe87cc2e227da  rubygems-0.9.4.tgz',
+      :dir => version,
+      :url => "http://rubyforge.org/frs/download.php/20989/rubygems-0.9.4.tgz",
+      :unpack => "tar zxf #{version}.tgz;",
+      :install => '/usr/bin/ruby1.8 setup.rb;'
+    }
+    deprec2.download_src(src_package, src_dir)
+    deprec2.install_from_src(src_package, src_dir)
+    gem2.upgrade
+    gem2.update_system
+  end
+  
 end
