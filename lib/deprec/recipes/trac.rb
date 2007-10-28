305b5f11414b08fbb0af1547dae88a69ae330506
mbailey <mbailey@479ab18a-db23-0410-b897-fa3724b950b0>
2007-10-28 07:09:21 +0000

    lots of changes - almost finished!
    git-svn-id: svn://scm.deprecated.org/deprec/trunk@143 479ab18a-db23-0410-b897-fa3724b950b0

--- lib/deprec/recipes/trac.rb
+++ lib/deprec/recipes/trac.rb
@@ -1,35 +1,27 @@
 Capistrano::Configuration.instance(:must_exist).load do 
-  namespace :deprec do
-    namespace :trac do
-      
-  set(:trac_backup_dir) { File.join(backup_dir, 'trac') }
-      
-      
-  set(:trac_home_url) { 'http://' + domain.sub(/^.*?\./, 'trac.') + '/' }
-   
-  set(:trac_password_file)  { "#{trac_path}/conf/users.htdigest" }
-  set(:trac_pidfile) { "#{deploy_to}/shared/pids/trac.pid" }
+  namespace :deprec do namespace :trac do
+        
+  # Master tracd process for server
+  set :tracd_cmd, '/usr/local/bin/tracd'
   set :tracd_port, '9000'
-  set (:trac_path) do
-    exists?(:deploy_to) ? "#{deploy_to}/trac" : Capistrano::CLI.ui.ask('path to trac config')
-  end
-  set (:trac_account) do
-    Capistrano::CLI.prompt('enter new trac user account name')
-  end  
+  set :tracd_pidfile, '/var/run/tracd.pid'
+  
+  # Settings for this projects trac instance
+  set(:trac_backup_dir) { "#{backup_dir}/trac" }
+  set(:trac_path) { exists?(:deploy_to) ? "#{deploy_to}/trac" : Capistrano::CLI.ui.ask('path to trac config') }
+  set(:tracd_parent_dir) { "#{trac_path}/projects" }
+  set(:trac_password_file)  { "#{trac_path}/conf/users.htdigest" }
+  set(:trac_account) { Capistrano::CLI.prompt('enter new trac user account name') }
   set :trac_passwordfile_exists, true # hack - should check on remote system instead
-  
   set(:trac_header_logo_link) { trac_home_url }
-  
+  # We will symlink each projects trac dir into this dir for tracd to find
+
   # project
   set(:trac_home_url) { 'http://' + domain.sub(/^.*?\./, 'trac.') + '/' }
   set(:trac_desc) { application } 
   
-  task :default do
-    puts trac_desc
-  end
-  
-  
-  # notification
+  # Settings only used for generating trac.ini for this project
+  # - notification
   set :trac_always_notify_owner, false
   set :trac_always_notify_reporter, false
   set :trac_always_notify_updater, true
@@ -47,12 +39,11 @@ Capistrano::Configuration.instance(:must_exist).load do
   set :trac_use_public_cc, false
   set :trac_use_short_addr, false
   set :trac_use_tls, false  
-  
+  # - other
   set(:trac_base_url) { trac_home_url }
   
-  
   desc "Install trac on server"
-  task :trac_install, :roles => :scm do
+  task :install, :roles => :scm do
     version = 'trac-0.10.4'
     set :src_package, {
       :file => version + '.tar.gz',   
@@ -63,9 +54,9 @@ Capistrano::Configuration.instance(:must_exist).load do
       :install => 'python ./setup.py install --prefix=/usr/local;'
     }
     enable_universe
-    apt.install( {:base => %w(python-sqlite sqlite python-clearsilver)}, :stable )
-    deprec.download_src(src_package, src_dir)
-    deprec.install_from_src(src_package, src_dir)
+    apt.install( {:base => %w(build-essential wget python-sqlite sqlite python-clearsilver)}, :stable )
+    deprec2.download_src(src_package, src_dir)
+    deprec2.install_from_src(src_package, src_dir)
   end
   
   # desc "Remove trac from server"
@@ -73,59 +64,125 @@ Capistrano::Configuration.instance(:must_exist).load do
     # not implemented
   end
   
-  task :trac_create_pid_dir, :roles => :scm do
-    deprec.mkdir(File.dirname(trac_pidfile))
+  SYSTEM_CONFIG_FILES[:trac] = [
+    {:template => 'tracd-init.erb',
+     :path => '/etc/init.d/tracd',
+     :mode => '0755',
+     :owner => 'root:root'}
+   ]
+  
+  PROJECT_CONFIG_FILES[:trac] = [
+    {:template => 'trac.ini.erb',
+     :path => "conf/trac.ini",
+     :mode => '0644',
+     :owner => 'root:root'}
+  ]
+  
+  desc "Generate config files for trac"
+  task :config_gen, :roles => :scm do
+    config_gen_system
+    config_gen_project
   end
   
-  task :trac_setup, :roles => :scm do
-    trac_init
-    trac_config
+  task :config_gen_system, :roles => :scm do
+    SYSTEM_CONFIG_FILES[:trac].each do |file|
+      deprec2.render('trac', file[:template], file[:path])
+    end
+  end
+  
+  task :config_gen_project, :roles => :scm do
+    PROJECT_CONFIG_FILES[:trac].each do |file|
+      deprec2.render('trac', file[:template], file[:path])
+    end
+  end
+  
+  desc "Push trac config files to server"
+  task :config, :roles => :scm do
+    config_system
+    config_project
+  end
+  
+  task :config_system, :roles => :scm do
+    deprec2.push_configs(:trac, SYSTEM_CONFIG_FILES[:trac])
+  end
+  
+  task :config_project, :roles => :scm do
+    deprec2.push_configs(:trac, PROJECT_CONFIG_FILES[:trac])
+  end
+
+  desc "Initialize the trac db for this project"
+  task :setup, :roles => :scm do
+    config_gen_project
+    config_project
+    init
+    set_default_permissions 
     # create trac account for current user 
     set :trac_account, user
     set :trac_passwordfile_exists, false # hack - should check on remote system instead
-    trac_user_add
-    
-    trac_create_pid_dir
+    user_add
+    create_pid_dir
   end
   
-  task :trac_init, :roles => :scm do
+  task :init, :roles => :scm do
     sudo "trac-admin #{trac_path} initenv #{application} sqlite:db/trac.db svn #{repos_root} /usr/local/share/trac/templates"
-    trac_set_default_permissions 
   end
   
-  task :trac_set_default_permissions, :roles => :scm do
-    trac_anonymous_disable
-    trac_authenticated_enable
+  task :set_default_permissions, :roles => :scm do
+    anonymous_disable
+    authenticated_enable
   end
   
-  # desc "disable anonymous access to everything"
-  task :trac_anonymous_disable, :roles => :scm do
-    sudo "trac-admin #{trac_path} permission remove anonymous '*'"
+  task :start, :roles => :scm do
+    sudo "/etc/init.d/tracd start"
+    sudo "/etc/init.d/httpd restart"
+  end
+
+  task :stop, :roles => :scm do
+    sudo "/etc/init.d/tracd stop"
+  end
+
+  task :activate, :roles => :scm do
+    activate_system
+    activate_project
   end
   
-  # desc "enable authenticated users access to everything"
-  task :trac_authenticated_enable, :roles => :scm do
-    sudo "trac-admin #{trac_path} permission add authenticated TRAC_ADMIN"
+  task :activate_system, :roles => :scm do
+    sudo "update-rc.d tracd defaults"
   end
   
-  task :trac_config, :roles => :scm do
-    deprec.render_template_to_file('trac/trac.ini.erb', "#{trac_path}/conf/trac.ini")
+  task :activate_project, :roles => :scm do
+    sudo "update-rc.d tracd defaults"
   end
   
-  task :trac_start, :roles => :scm do
-    # XXX enable this for cap2
-    # XXX run "echo point your browser to http://$CAPISTRANO:HOST$:#{tracd_port}/trac"  
-    auth_string = "--auth=*,#{trac_password_file},#{application}"
-    sudo "tracd #{auth_string} --daemonize --single-env --port=#{tracd_port} --pidfile=#{trac_pidfile} #{trac_path}"
+  task :deactivate, :roles => :scm do
+    deactivate_system
+    unlink_project
   end
   
-  task :trac_stop, :roles => :scm do
-    sudo "kill `cat #{trac_pidfile}` >/dev/null 2>&1"
-    sudo "rm -f #{trac_pidfile}"
+  task :deactivate_system, :roles => :scm do
+    sudo "update-rc.d -f tracd remove"
   end
   
+  desc "Create backup of trac repository"
+  task :backup, :roles => :web do
+    # http://trac.edgewall.org/wiki/TracBackup
+    timestamp = Time.now.utc.strftime("%Y%m%d%H%M%S")
+    dest_dir = File.join(trac_backup_dir, "trac_#{application}_#{timestamp}")
+    sudo "trac-admin #{trac_path} hotcopy #{dest_dir}"
+  end
+  
+  desc "Restore trac repository from backup"
+  task :restore, :roles => :web do
+    # prompt user to select from list of locally stored backups
+    # tracd_stop
+    # copy out backup
+  end
+  
+  #
+  # Service specific tasks for end users
+  #
   desc "create a trac user"
-  task :trac_user_add, :roles => :scm do
+  task :user_add, :roles => :scm do
     create_file = trac_passwordfile_exists ? '' : ' -c '
     htdigest = '/usr/local/apache2/bin/htdigest'
     # XXX check if htdigest file exists and add '-c' option if not
@@ -135,22 +192,35 @@ Capistrano::Configuration.instance(:must_exist).load do
   end
   
   desc "list trac users"
-  task :trac_list_users, :roles => :scm do
+  task :list_users, :roles => :scm do
     sudo "cat #{trac_path}/conf/users.htdigest"
   end
   
-  desc "create backup of trac repository"
-  task :backup, :roles => :web do
-    # http://trac.edgewall.org/wiki/TracBackup
-    timestamp = Time.now.utc.strftime("%Y%m%d%H%M%S")
-    dest_dir = File.join(trac_backup_dir, "trac_#{application}_#{timestamp}"
-    sudo "trac-admin #{trac_path} hotcopy #{dest_dir)}"
+  # desc "disable anonymous access to everything"
+  task :anonymous_disable, :roles => :scm do
+    sudo "trac-admin #{trac_path} permission remove anonymous '*'"
   end
   
-  task :restore, :roles => :web do
-    # prompt user to select from list of locally stored backups
-    # tracd_stop
-    # copy out backup
+  # desc "enable authenticated users access to everything"
+  task :authenticated_enable, :roles => :scm do
+    sudo "trac-admin #{trac_path} permission add authenticated TRAC_ADMIN"
+  end
+  
+  #
+  # Helper tasks used by other tasks
+  #
+  task :symlink_project, :roles => :scm do
+    link = File.join(tracd_parent_dir, application)
+    sudo "ln -sf #{link}"
+  end
+  
+  task :unlink_project, :roles => :scm do
+    link = File.join(tracd_parent_dir, application)
+    sudo "test -h #{link} && unlink #{link} || true"
+  end
+
+  task :create_pid_dir, :roles => :scm do
+    deprec.mkdir(File.dirname(tracd_pidfile))
   end
   
 end end
