d8649ff7f8d3e93c4a2ba6000176d4ede70a1680
mbailey <mbailey@479ab18a-db23-0410-b897-fa3724b950b0>
2007-11-26 05:32:04 +0000

    commented out as postfix not currently being used
    git-svn-id: svn://scm.deprecated.org/deprec/trunk@184 479ab18a-db23-0410-b897-fa3724b950b0

--- lib/deprec/recipes/postfix.rb
+++ lib/deprec/recipes/postfix.rb
@@ -1,15 +1,15 @@
-Capistrano::Configuration.instance(:must_exist).load do 
-
-  namespace :deprec do namespace :nginx do
-      
-  #Craig: I've kept this generic rather than calling the task setup postfix. 
-  # if people want other smtp servers, it could be configurable
-  desc "install and configure postfix"
-  task :setup_smtp_server do
-    install_postfix
-    set :postfix_destination_domains, [domain] + apache_server_aliases
-    deprec.render_template_to_file('postfix_main', '/etc/postfix/main.cf')
-  end
-
-  end end
-end
+# Capistrano::Configuration.instance(:must_exist).load do 
+# 
+#   namespace :deprec do namespace :nginx do
+#       
+#   #Craig: I've kept this generic rather than calling the task setup postfix. 
+#   # if people want other smtp servers, it could be configurable
+#   desc "install and configure postfix"
+#   task :setup_smtp_server do
+#     install_postfix
+#     set :postfix_destination_domains, [domain] + apache_server_aliases
+#     deprec.render_template_to_file('postfix_main', '/etc/postfix/main.cf')
+#   end
+# 
+#   end end
+# end
