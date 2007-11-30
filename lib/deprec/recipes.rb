393ccd9bfbf72dfc61a9ae47520bf2b2aef1038a
mbailey <mbailey@479ab18a-db23-0410-b897-fa3724b950b0>
2007-11-30 09:48:32 +0000

    renamed user.rb to users.rb as it clashed with the user keyword added network.rb for manipulating network on host
    git-svn-id: svn://scm.deprecated.org/deprec/trunk@186 479ab18a-db23-0410-b897-fa3724b950b0

--- lib/deprec/recipes.rb
+++ lib/deprec/recipes.rb
@@ -1,11 +1,10 @@
 unless Capistrano::Configuration.respond_to?(:instance)
   abort "deprec2 requires Capistrano 2"
 end
-require "#{File.dirname(__FILE__)}/recipes/sources"
 require "#{File.dirname(__FILE__)}/recipes/deprec"
 require "#{File.dirname(__FILE__)}/recipes/deprecated"
 require "#{File.dirname(__FILE__)}/recipes/canonical"
-require "#{File.dirname(__FILE__)}/recipes/user"
+require "#{File.dirname(__FILE__)}/recipes/users"
 require "#{File.dirname(__FILE__)}/recipes/ssh"
 require "#{File.dirname(__FILE__)}/recipes/nginx"
 require "#{File.dirname(__FILE__)}/recipes/apache"
@@ -19,6 +18,7 @@ require "#{File.dirname(__FILE__)}/recipes/mysql"
 require "#{File.dirname(__FILE__)}/recipes/postgres"
 require "#{File.dirname(__FILE__)}/recipes/postfix"
 require "#{File.dirname(__FILE__)}/recipes/memcache"
+require "#{File.dirname(__FILE__)}/recipes/network"
 require "#{File.dirname(__FILE__)}/recipes/vmware"
 
 # this will be pulled out into ubuntu plugin
