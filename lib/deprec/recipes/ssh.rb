1b012ab8cb4c9d850b57391439362242bbe718ce
mbailey <mbailey@479ab18a-db23-0410-b897-fa3724b950b0>
2007-12-04 12:27:16 +0000

    generate sshd_config
    git-svn-id: svn://scm.deprecated.org/deprec/trunk@209 479ab18a-db23-0410-b897-fa3724b950b0

--- lib/deprec/recipes/ssh.rb
+++ lib/deprec/recipes/ssh.rb
@@ -1,6 +1,25 @@
 Capistrano::Configuration.instance(:must_exist).load do 
   namespace :deprec do
     namespace :ssh do
+      
+      SYSTEM_CONFIG_FILES[:ssh] = [
+        
+        {:template => "sshd_config.erb",
+         :path => '/etc/ssh/sshd_config_foo',
+         :mode => '0644',
+         :owner => 'root:root'}
+      ]
+      
+      task :config_gen do        
+        SYSTEM_CONFIG_FILES[:ssh].each do |file|
+          deprec2.render_template(:ssh, file)
+        end
+      end
+      
+      desc "Push apache config files to server"
+      task :config do
+        deprec2.push_configs(:ssh, SYSTEM_CONFIG_FILES[:ssh])
+      end
 
       desc "Sets up authorized_keys file on remote server"
       task :setup_keys do
