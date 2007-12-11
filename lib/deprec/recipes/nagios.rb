55a9939f522a3ca57a659f3f4bbafbae22085dd3
mbailey <mbailey@479ab18a-db23-0410-b897-fa3724b950b0>
2007-12-11 14:58:47 +0000

    It installs nagios, nagiosplugins and gets it going. Wow.
    git-svn-id: svn://scm.deprecated.org/deprec/trunk@228 479ab18a-db23-0410-b897-fa3724b950b0

--- lib/deprec/recipes/nagios.rb
+++ lib/deprec/recipes/nagios.rb
@@ -2,46 +2,42 @@ Capistrano::Configuration.instance(:must_exist).load do
   namespace :deprec do
     namespace :nagios do
       
+      set :nagios_user, 'nagios'
+      set :nagios_group, 'nagios'
+      set :nagios_cmd_group, 'nagcmd' # Allow external commands to be submitted through the web interface
+      
       SRC_PACKAGES[:nagios] = {
-        :filename => 'httpd-2.2.6.tar.gz',   
-        :md5sum => "d050a49bd7532ec21c6bb593b3473a5d  httpd-2.2.6.tar.gz", 
-        :dir => 'httpd-2.2.6',  
-        :url => "http://www.apache.org/dist/httpd/httpd-2.2.6.tar.gz",
-        :unpack => "tar zxf httpd-2.2.6.tar.gz;",
+        :filename => 'nagios-3.0b7.tar.gz',   
+        :md5sum => "3c3aaeddff040ba57c3c43553524b13f  nagios-3.0b7.tar.gz", 
+        :dir => 'nagios-3.0b7',  
+        :url => "http://osdn.dl.sourceforge.net/sourceforge/nagios/nagios-3.0b7.tar.gz",
+        :unpack => "tar zxfv nagios-3.0b7.tar.gz;",
         :configure => %w(
-          ./configure
-          --enable-mods-shared=all
-          --enable-proxy 
-          --enable-proxy-balancer 
-          --enable-proxy-http 
-          --enable-rewrite  
-          --enable-cache 
-          --enable-headers 
-          --enable-ssl 
-          --enable-deflate 
-          --with-included-apr   #_so_this_recipe_doesn't_break_when_rerun
-          --enable-dav          #_for_subversion_
-          --enable-so           #_for_subversion_
+          ./configure 
+          --with-command-group=nagcmd
           ;
           ).reject{|arg| arg.match '#'}.join(' '),
-        :make => 'make;',
-        :install => 'make install;',
-        :post_install => 'install -b support/apachectl /etc/init.d/httpd;'
+        :make => 'make all;',
+        :install => 'make install install-init install-commandmode'
       }
       
       desc "Install nagios"
       task :install do
         install_deps
+        deprec2.groupadd(nagios_group)
+        deprec2.useradd(nagios_user, :group => nagios_group, :homedir => false)
+        deprec2.groupadd(nagios_cmd_group)
+        deprec2.add_user_to_group(nagios, nagios_cmd_group)
+        deprec2.add_user_to_group(nagios, apache_user)
+        deprec2.mkdir('/usr/local/nagios/etc', :owner => "#{nagios_user}.#{nagios_group}", :via => :sudo)
+        deprec2.mkdir('/usr/local/nagios/objects', :owner => "#{nagios_user}.#{nagios_group}", :via => :sudo)
         deprec2.download_src(SRC_PACKAGES[:nagios], src_dir)
         deprec2.install_from_src(SRC_PACKAGES[:nagios], src_dir)
-        setup_vhost_dir
-        install_index_page
       end
          
-      # install dependencies for nagios
+      # Install dependencies for nagios
       task :install_deps do
-        puts "This function should be overridden by your OS plugin!"
-        apt.install( {:base => %w(build-essential zlib1g-dev zlib1g openssl libssl-dev)}, :stable )
+        apt.install( {:base => %w(build-essential)}, :stable )
       end
       
       SYSTEM_CONFIG_FILES[:nagios] = [
@@ -51,6 +47,11 @@ Capistrano::Configuration.instance(:must_exist).load do
         :mode => '0664',
         :owner => 'nagios:nagios'},
 
+        {:template => 'resource.cfg.erb',
+        :path => '/usr/local/nagios/etc/resource.cfg',
+        :mode => '0660',
+        :owner => 'nagios:nagios'},
+        
         {:template => 'cgi.cfg.erb',
         :path => '/usr/local/nagios/etc/cgi.cfg',
         :mode => '0664',
@@ -89,12 +90,17 @@ Capistrano::Configuration.instance(:must_exist).load do
         {:template => 'hosts.cfg.erb',
         :path => '/usr/local/nagios/etc/objects/hosts.cfg',
         :mode => '0664',
-        :owner => 'nagios:nagios'}
+        :owner => 'nagios:nagios'},
         
+        {:template => 'nagios_apache_vhost.conf.erb',
+         :path => "conf/nagios_apache_vhost.conf",
+         :mode => '0644',
+         :owner => 'root:root'}
+      
       ]
 
       PROJECT_CONFIG_FILES[:nagios] = [
-        
+
       ]
 
       desc "Generate configuration file(s) for nagios from template(s)"
@@ -154,14 +160,14 @@ Capistrano::Configuration.instance(:must_exist).load do
         send(run_method, "/etc/init.d/nagios check")
       end
 
-      desc "Set apache to start on boot"
+      desc "Set Nagios to start on boot"
       task :activate, :roles => :web do
-        send(run_method, "update-rc.d httpd defaults")
+        send(run_method, "update-rc.d nagios defaults")
       end
       
-      desc "Set apache to not start on boot"
+      desc "Set Nagios to not start on boot"
       task :deactivate, :roles => :web do
-        send(run_method, "update-rc.d -f httpd remove")
+        send(run_method, "update-rc.d -f nagios remove")
       end
       
       task :backup, :roles => :web do
@@ -171,7 +177,42 @@ Capistrano::Configuration.instance(:must_exist).load do
       task :restore, :roles => :web do
         # not yet implemented
       end
+      
+      #
+      # Service specific tasks
+      #
+      
+      # XXX quick and dirty - clean up later
+      desc "Grant a user access to the web interface"
+      task :htpass, :roles => :nagios do
+        target_user = Capistrano::CLI.ui.ask "Userid" do |q|
+          q.default = 'nagiosadmin'
+        end
+        system "htpasswd config/nagios/usr/local/nagios/etc/htpasswd.users #{target_user}"
+      end
     
     end
+    
+    
+    SRC_PACKAGES[:nagios_plugins] = {
+      :filename => 'nagios-plugins-1.4.10.tar.gz',   
+      :md5sum => "c67841223864ae1626ab2adb2f0b4c9d  nagios-plugins-1.4.10.tar.gz", 
+      :dir => 'nagios-plugins-1.4.10',  
+      :url => "wget http://osdn.dl.sourceforge.net/sourceforge/nagiosplug/nagios-plugins-1.4.10.tar.gz",
+      :unpack => "tar zxfv nagios-plugins-1.4.10.tar.gz;",
+      :configure => "./configure --with-nagios-user=#{nagios_user} --with-nagios-group=#{nagios_group};",
+      :make => 'make;',
+      :install => 'make install;'
+    }   
+          
+    namespace :nagios_plugins do
+    
+      task :install do
+        deprec2.download_src(SRC_PACKAGES[:nagios_plugins], src_dir)
+        deprec2.install_from_src(SRC_PACKAGES[:nagios_plugins], src_dir)        
+      end
+      
+    end
+    
   end
 end
