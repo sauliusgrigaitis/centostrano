a307945907115b987098e0fa6e4369f079023251
mbailey <mbailey@479ab18a-db23-0410-b897-fa3724b950b0>
2007-12-11 14:08:24 +0000

    brought example file up to date
    git-svn-id: svn://scm.deprecated.org/deprec/trunk@220 479ab18a-db23-0410-b897-fa3724b950b0

--- lib/deprec/recipes/example.rb
+++ lib/deprec/recipes/example.rb
@@ -2,17 +2,82 @@ Capistrano::Configuration.instance(:must_exist).load do
   namespace :deprec do
     namespace :example do
       
+      SRC_PACKAGES[:example] = {
+        :filename => 'example-1.2.3.tar.gz',   
+        :md5sum => "d050a49bd72222ec21c6bb593b3473a5d  example-1.2.3.tar.gz", 
+        :dir => 'example-1.2.3',  
+        :url => "http://www.example.org/dist/example/example-1.2.3.tar.gz",
+        :unpack => "tar zxf example-1.2.3.tar.gz;",
+        :configure => %w(
+          ./configure
+          --enable-mods-shared=all
+          --enable-proxy 
+          ;
+          ).reject{|arg| arg.match '#'}.join(' '),
+        :make => 'make;',
+        :install => 'make install;',
+        :post_install => 'install -b support/apachectl /etc/init.d/httpd;'
+      }
+      
       desc "Install example"
       task :install, :roles => :web do
+        install_deps
       end
+      
+      task :install_deps do
+        apt.install( {:base => %w(build-essential zlib1g-dev)}, :stable )
+      end
+      
+      SYSTEM_CONFIG_FILES[:example] = [
+        
+        {:template => "example.conf.erb",
+         :path => '/etc/example/example.conf',
+         :mode => '0755',
+         :owner => 'root:root'}
+         
+      ]
+
+      PROJECT_CONFIG_FILES[:example] = [
+        
+        {:template => "example.conf.erb",
+         :path => 'conf/example.conf',
+         :mode => '0755',
+         :owner => 'root:root'}
+      ]
+      
       
       desc "Generate configuration file(s) for XXX from template(s)"
       task :config_gen do
+        config_gen_system
+        config_gen_project
+      end
+
+      task :config_gen_system do
+        SYSTEM_CONFIG_FILES[:example].each do |file|
+          deprec2.render_template(:example, file)
+        end
+      end
+
+      task :config_gen_project do
+        PROJECT_CONFIG_FILES[:example].each do |file|
+          deprec2.render_template(:example, file)
+        end
       end
       
       desc 'Deploy configuration files(s) for XXX' 
       task :config, :roles => :web do
+        config_system
+        config_project
       end
+
+      task :config_system, :roles => :web do
+        deprec2.push_configs(:example, SYSTEM_CONFIG_FILES[:example])
+      end
+
+      task :config_project, :roles => :web do
+        deprec2.push_configs(:example, PROJECT_CONFIG_FILES[:example])
+      end
+      
       
       task :start, :roles => :web do
       end
