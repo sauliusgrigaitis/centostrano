362a1ab94f62ee13ee933c31031edcde17bc916e
mbailey <mbailey@479ab18a-db23-0410-b897-fa3724b950b0>
2007-12-11 14:58:03 +0000

    not ready yet.
    git-svn-id: svn://scm.deprecated.org/deprec/trunk@227 479ab18a-db23-0410-b897-fa3724b950b0

--- lib/deprec/recipes/postfix.rb
+++ lib/deprec/recipes/postfix.rb
@@ -1,4 +1,65 @@
-# Capistrano::Configuration.instance(:must_exist).load do 
+Capistrano::Configuration.instance(:must_exist).load do 
+  # XXX not complete
+  namespace :deprec do
+    namespace :postfix do
+      
+      desc "Install example"
+      task :install, :roles => :web do
+        install_deps
+      end
+      
+      task :install_deps do
+        apt.install( {:base => %w(build-essential postfix)}, :stable )
+      end
+      
+      SYSTEM_CONFIG_FILES[:postfix] = [
+        
+        {:template => "example.conf.erb",
+         :path => '/etc/example/example.conf',
+         :mode => '0755',
+         :owner => 'root:root'}
+         
+      ]
+      
+      desc "Generate configuration file(s) for XXX from template(s)"
+      task :config_gen do
+        SYSTEM_CONFIG_FILES[:postfix].each do |file|
+          deprec2.render_template(:postfix, file)
+        end
+      end
+      
+      desc 'Deploy configuration files(s) for XXX' 
+      task :config, :roles => :mail do
+        deprec2.push_configs(:postfix, SYSTEM_CONFIG_FILES[:postfix])
+      end
+      
+      task :start, :roles => :web do
+      end
+      
+      task :stop, :roles => :web do
+      end
+      
+      task :restart, :roles => :web do
+      end
+      
+      task :activate, :roles => :web do
+      end  
+      
+      task :deactivate, :roles => :web do
+      end
+      
+      task :backup, :roles => :web do
+      end
+      
+      task :restore, :roles => :web do
+      end
+      
+    end
+  end
+end
+      
+      
+      # Capistrano::Configuration.instance(:must_exist).load do 
 # 
 #   namespace :deprec do namespace :nginx do
 #       
