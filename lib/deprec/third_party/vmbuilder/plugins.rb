78bed3c60cc1385fb428c0f3dd3a7e751715e7fe
mbailey <mbailey@479ab18a-db23-0410-b897-fa3724b950b0>
2008-01-17 14:32:03 +0000

    Phasing out third_party dir
    git-svn-id: svn://scm.deprecated.org/deprec/trunk@279 479ab18a-db23-0410-b897-fa3724b950b0

--- lib/deprec/third_party/vmbuilder/plugins.rb
+++
@@ -1,8 +0,0 @@
-# plugins.rb: Load all the Capistrano Plugins in the plugins directory.
-
-require 'rubygems'
-
-# Load plugins from the plugins directory
-Dir["#{File.dirname(__FILE__)}/plugins/*.rb"].sort.each do |plugin|
-  load plugin
-end
