7e49a7a3d41194e173c2dd647b3f58c3007e1ae8
mbailey <mbailey@479ab18a-db23-0410-b897-fa3724b950b0>
2008-01-17 14:11:30 +0000

    removed _as_root tasks
    git-svn-id: svn://scm.deprecated.org/deprec/trunk@261 479ab18a-db23-0410-b897-fa3724b950b0

--- lib/deprec/recipes/deprecated.rb
+++ lib/deprec/recipes/deprecated.rb
@@ -7,22 +7,12 @@ Capistrano::Configuration.instance(:must_exist).load do
   
   task :setup_admin_account do
     puts "The deprec task setup_admin_account has been deprecated."
-    puts "Please use the replacement version deprec:users:add_admin"
-  end
-  
-  task :setup_admin_account_as_root do
-    puts "The deprec task setup_admin_account_as_root has been deprecated."
-    puts "Please use the replacement version deprec:users:add_admin_as_root"
+    puts "Please use the replacement version deprec:users:add"
   end
   
   task :change_root_password do
     puts "The deprec task change_root_password has been deprecated."
     puts "Please use the replacement version deprec:users:passwd"
-  end
-  
-  task :change_root_password_as_root do
-    puts "The deprec task change_root_password_as_root has been deprecated."
-    puts "Please use the replacement version deprec:users:passwd_as_root"
   end
   
 end
