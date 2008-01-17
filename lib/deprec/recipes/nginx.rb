dc54687e8f54182df7b4cd5e24875cebfdb0b9e3
mbailey <mbailey@479ab18a-db23-0410-b897-fa3724b950b0>
2008-01-17 14:14:22 +0000

    ready to use
    git-svn-id: svn://scm.deprecated.org/deprec/trunk@264 479ab18a-db23-0410-b897-fa3724b950b0

--- lib/deprec/recipes/nginx.rb
+++ lib/deprec/recipes/nginx.rb
@@ -2,6 +2,8 @@ Capistrano::Configuration.instance(:must_exist).load do
   namespace :deprec do namespace :nginx do
         
   set :nginx_server_name, nil
+  set :nginx_user,  'nginx'
+  set :nginx_group, 'nginx'
   
   # Configuration summary
   #   + threads are not used
@@ -21,49 +23,59 @@ Capistrano::Configuration.instance(:must_exist).load do
   #   nginx http proxy temporary files: "/usr/local/nginx/proxy_temp"
   #   nginx http fastcgi temporary files: "/usr/local/nginx/fastcgi_temp"
     
+  SRC_PACKAGES[:nginx] = {
+    :filename => 'nginx-0.5.34.tar.gz',   
+    :md5sum => "8f7d3efcd7caaf1f06e4d95dfaeac238  nginx-0.5.34.tar.gz", 
+    :dir => 'nginx-0.5.34',  
+    :url => "http://sysoev.ru/nginx/nginx-0.5.34.tar.gz",
+    :unpack => "tar zxf nginx-0.5.34.tar.gz;",
+    :configure => %w(
+      ./configure
+      --sbin-path=/usr/local/sbin
+      --with-http_ssl_module
+      ;
+      ).reject{|arg| arg.match '#'}.join(' '),
+    :make => 'make;',
+    :install => 'make install;'
+  }
   
-  # http://sysoev.ru/nginx/nginx-0.5.33.tar.gz
   desc "Install nginx"
   task :install do
-    version = 'nginx-0.5.33'
-    set :src_package, {
-      :file => version + '.tar.gz',   
-      :md5sum => "a78be74b4fd8e009545ef02488fcac86  #{version}.tar.gz", 
-      :dir => version,  
-      :url => "http://sysoev.ru/nginx/#{version}.tar.gz",
-      :unpack => "tar zxf #{version}.tar.gz;",
-      :configure => %w(
-        ./configure
-        --with-http_ssl_module
-        --with-http_dav_module
-        ;
-        ).reject{|arg| arg.match '#'}.join(' '),
-      :make => 'make;',
-      :install => 'make install;',
-      :post_install => ''
-    }
     install_deps
-    deprec2.download_src(src_package, src_dir)
-    deprec2.install_from_src(src_package, src_dir)
+    deprec2.download_src(SRC_PACKAGES[:nginx], src_dir)
+    deprec2.install_from_src(SRC_PACKAGES[:nginx], src_dir)
+    create_nginx_user
+    # setup_vhost_dir     # XXX not done yet
+    # install_index_page  # XXX not done yet
   end
   
-  # install dependencies for apache
+  # install dependencies for nginx
   task :install_deps do
-    puts "This function should be overridden by your OS plugin!"
-    apt.install( {:base => %w(build-essential zlib1g-dev zlib1g openssl libssl-dev libpcre3-dev libgcrypt11-dev)}, :stable )
+    apt.install( {:base => %w(libpcre3 libpcre3-dev libpcrecpp0 libssl-dev zlib1g-dev)}, :stable )
+    # do we need libgcrypt11-dev?
+  end
+  
+  task :create_nginx_user do
+    deprec2.groupadd(nginx_group)
+    deprec2.useradd(nginx_user, :group => nginx_group, :homedir => false)
   end
     
   SYSTEM_CONFIG_FILES[:nginx] = [
     
-    {:template => 'nginx-init-gutsy',
+    {:template => 'nginx-init-script',
      :path => '/etc/init.d/nginx',
      :mode => '0755',
      :owner => 'root:root'},
      
-     {:template => 'nginx.conf-gutsy.erb',
-      :path => "/usr/local/nginx/conf/nginx.conf",
-      :mode => '0644',
-      :owner => 'root:root'}
+    {:template => 'nginx.conf.erb',
+     :path => "/usr/local/nginx/conf/nginx.conf",
+     :mode => '0644',
+     :owner => 'root:root'},
+      
+    {:template => 'mime.types.erb',
+     :path => "/usr/local/nginx/conf/mime.types",
+     :mode => '0644',
+     :owner => 'root:root'}
   ]
   
   PROJECT_CONFIG_FILES[:nginx] = [
@@ -76,13 +88,8 @@ Capistrano::Configuration.instance(:must_exist).load do
   The can be pushed to the server with the :config task.
   DESC
   task :config_gen do
-    config_gen_system
-    # config_gen_project
-  end
-  
-  task :config_gen_system do
     SYSTEM_CONFIG_FILES[:nginx].each do |file|
-      render_template(:nginx, file)
+      deprec2.render_template(:nginx, file)
     end
   end
   
@@ -94,39 +101,35 @@ Capistrano::Configuration.instance(:must_exist).load do
   
   desc "Push trac config files to server"
   task :config, :roles => :web do
-    config_system
-    # config_project
-  end
-  
-  task :config_system, :roles => :web do
     deprec2.push_configs(:nginx, SYSTEM_CONFIG_FILES[:nginx])
   end
-  
-  # task :config_project, :roles => :web do
-  #   deprec2.push_configs(:nginx, PROJECT_CONFIG_FILES[:nginx])
-  # end
-    
+
   desc "Start Nginx"
   task :start, :roles => :web do
-    puts "starting nginx"
+    send(run_method, "/etc/init.d/nginx start")
   end
-  
+
   desc "Stop Nginx"
   task :stop, :roles => :web do
-    puts "stopping nginx"
+    send(run_method, "/etc/init.d/nginx stop")
   end
-  
+
   desc "Restart Nginx"
   task :restart, :roles => :web do
-    stop
-    start
+    send(run_method, "/etc/init.d/nginx restart")
   end
-  
+
+  desc "Reload Nginx"
+  task :reload, :roles => :web do
+    send(run_method, "/etc/init.d/nginx reload")
+  end
+   
   desc <<-DESC
     Activate nginx start scripts on server.
     Setup server to start nginx on boot.
   DESC
   task :activate, :roles => :web do
+    send(run_method, "update-rc.d nginx defaults")
   end
   
   desc <<-DESC
@@ -134,6 +137,7 @@ Capistrano::Configuration.instance(:must_exist).load do
     Setup server to start nginx on boot.
   DESC
   task :deactivate, :roles => :web do
+    send(run_method, "update-rc.d -f nginx remove")
   end
   
   task :backup, :roles => :web do
