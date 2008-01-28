eb9c55c48b514e25600a09fa9fec48e25508b825
mbailey <mbailey@479ab18a-db23-0410-b897-fa3724b950b0>
2008-01-28 13:10:41 +0000

    now working
    git-svn-id: svn://scm.deprecated.org/deprec/trunk@310 479ab18a-db23-0410-b897-fa3724b950b0

--- lib/deprec/recipes/rails.rb
+++ lib/deprec/recipes/rails.rb
@@ -1,29 +1,32 @@
 Capistrano::Configuration.instance(:must_exist).load do 
 
-  set :database_yml_in_scm, true
+  set :database_yml_in_scm, false
   set :app_symlinks, nil
   set :rails_env, 'production'
-
-  # run "rake db:create" # waiting for Rails2
-
 
   # Hook into the default capistrano deploy tasks
   before 'deploy:setup', :except => { :no_release => true } do
     top.deprec.rails.setup_user_perms
     top.deprec.rails.setup_paths
+    top.deprec.rails.setup_shared_dirs
+  end
+  
+  # Override default cap task using sudo to create dir
+  namespace :deploy do
+    task :setup, :except => { :no_release => true } do
+      dirs = [deploy_to, releases_path, shared_path]
+      dirs += %w(system log pids).map { |d| File.join(shared_path, d) }
+      sudo "sh -c 'umask 02 && mkdir -p #{dirs.join(' ')}'"
+    end
   end
 
   after 'deploy:setup', :except => { :no_release => true } do
-    top.deprec.rails.setup_paths
-    top.deprec.rails.setup_symlinks
     top.deprec.rails.setup_servers
   end
 
-  after 'deploy:update_code', :roles => :app do
-    top.deprec.rails.symlink_database_yml unless database_yml_in_scm
-  end
-
   after 'deploy:symlink', :roles => :app do
+    top.deprec.rails.symlink_shared_dirs
+    top.deprec.rails.symlink_database_yml unless database_yml_in_scm
     top.deprec.mongrel.set_perms_for_mongrel_dirs
   end
 
@@ -65,8 +68,16 @@ Capistrano::Configuration.instance(:must_exist).load do
         gem2.install 'sqlite3-ruby'
         gem2.install 'mysql'
         gem2.install 'rails'
+        gem2.install 'rspec' # seems to be required to run rake db:migrate (???)
         # gem2.install 'builder' # XXX ? needed ?
+        install_gems_for_project
       end
+      
+      # task :install_gems_for_project do
+      #     if gems_for_project
+      #       gems_for_project.each { |gem| gem2.install(gem) }
+      #     end
+      # end
 
       task :config_gen do
         PROJECT_CONFIG_FILES[:nginx].each do |file|
@@ -89,32 +100,41 @@ Capistrano::Configuration.instance(:must_exist).load do
         deprec2.add_user_to_group(user, group)
       end
 
-      # setup extra paths required for deployment
-      task :setup_paths, :roles => :app do
-        # XXX make a function to create a group writable dir
-        sudo "test -d #{shared_path}/config || sudo mkdir -p #{shared_path}/config"
-        sudo "chgrp -R #{group} #{deploy_to}"
-        sudo "chmod -R g+w #{deploy_to}"
-      end
-
       # Setup database server.
       task :setup_db, :roles => :db, :only => { :primary => true } do
         top.deprec.mysql.setup
       end
 
-      desc "Setup public symlink directories"
-      task :setup_symlinks, :roles => [:app, :web] do
-        if app_symlinks
-          app_symlinks.each { |link| run "mkdir -p #{shared_path}/public/#{link}" }
+      # setup extra paths required for deployment
+      task :setup_paths, :roles => :app do
+        deprec2.mkdir(deploy_to, :mode => '0775', :group => group, :via => :sudo)
+        deprec2.mkdir(shared_path, :mode => '0775', :group => group, :via => :sudo)
+      end
+      
+      # Symlink list of files and dirs from shared to current
+      #
+      # XXX write up explanation
+      #
+      desc "Setup shared dirs"
+      task :setup_shared_dirs, :roles => [:app, :web] do
+        if shared_dirs
+          shared_dirs.each { |dir| deprec2.mkdir( "#{shared_path}/#{dir}", :via => :sudo ) }
         end
       end
-
-      desc "Link up any public directories."
-      task :symlink_public, :roles => [:app, :web] do
-        if app_symlinks
-          app_symlinks.each { |link| run "ln -nfs #{shared_path}/public/#{link} #{current_path}/public/#{link}" }
+      #
+      desc "Symlink shared dirs."
+      task :symlink_shared_dirs, :roles => [:app, :web] do
+        if shared_dirs
+          shared_dirs.each { |dir| run "ln -nfs #{shared_path}/#{dir} #{current_path}/#{dir}" }
         end
       end
+      
+      # desc "Symlink shared files."
+      # task :symlink_shared_files, :roles => [:app, :web] do
+      #   if shared_files
+      #     shared_files.each { |file| run "ln -nfs #{shared_path}/#{file} #{current_path}/#{file}" }
+      #   end
+      # end
 
       # database.yml stuff
       #
@@ -163,8 +183,7 @@ Capistrano::Configuration.instance(:must_exist).load do
 
       desc "Link in the production database.yml" 
       task :symlink_database_yml, :roles => :app do
-        # run "rm -f #{current_path}/config/database.yml"
-        run "ln -nfs #{deploy_to}/#{shared_dir}/config/database.yml #{release_path}/config/database.yml" 
+        run "ln -nfs #{shared_path}/config/database.yml #{release_path}/config/database.yml" 
       end
 
       desc <<-DESC
