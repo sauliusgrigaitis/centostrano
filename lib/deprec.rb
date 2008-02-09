4f9df75696d63f5b9796d890f95f46cb69f8f1a9
mbailey <mbailey@479ab18a-db23-0410-b897-fa3724b950b0>
2008-02-09 15:36:56 +0000

    renamed capistrano_extensions.rb
    git-svn-id: svn://scm.deprecated.org/deprec/trunk@349 479ab18a-db23-0410-b897-fa3724b950b0

--- lib/deprec.rb
+++ lib/deprec.rb
@@ -2,8 +2,7 @@ unless Capistrano::Configuration.respond_to?(:instance)
   abort "deprec2 requires Capistrano 2"
 end
 
-require "#{File.dirname(__FILE__)}/deprec/capistrano_extensions/deprec2_extensions"
-# require "#{File.dirname(__FILE__)}/third_party/vmbuilder/plugins/std"
+require "#{File.dirname(__FILE__)}/deprec/capistrano_extensions"
 require "#{File.dirname(__FILE__)}/deprec/vmbuilder_plugins/all"
 require "#{File.dirname(__FILE__)}/deprec/recipes"
 
