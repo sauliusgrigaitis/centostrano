ea70c25ef5be190cc9efa9322f0108478b054d79
mbailey <mbailey@479ab18a-db23-0410-b897-fa3724b950b0>
2008-02-09 15:37:35 +0000

    added more deprecation notices
    git-svn-id: svn://scm.deprecated.org/deprec/trunk@350 479ab18a-db23-0410-b897-fa3724b950b0

--- lib/deprec/recipes/deprecated.rb
+++ lib/deprec/recipes/deprecated.rb
@@ -6,14 +6,66 @@ Capistrano::Configuration.instance(:must_exist).load do
   
   # XXX use deprecated function to generate these dynamically
   
+  deprec2_isnt_backwards_compatible = <<-EOF
+    
+You've installed deprec2 but seem to be using a deprec1 command.
+
+You have two options:
+
+- install deprec-1.9.x and continue using deprec2
+
+  Instructions are available at http://www.deprec.org/
+
+- use deprec2
+
+EOF
+
+  cap2_warning = <<-EOF
+  
+You're using Capistrano 2 but using a deprecated cap1 command.
+
+EOF
+  
   task :setup_admin_account do
-    puts "The deprec task setup_admin_account has been deprecated."
-    puts "Please use the replacement version deprec:users:add"
+    puts deprec2_isnt_backwards_compatible  
+    puts "  Replace 'cap setup_admin_account' with 'cap deprec:users:add'"
+    puts
   end
   
   task :change_root_password do
-    puts "The deprec task change_root_password has been deprecated."
-    puts "Please use the replacement version deprec:users:passwd"
+    puts deprec2_isnt_backwards_compatible  
+    puts "  Replace 'cap change_root_password' with 'cap deprec:users:passwd'"
+    puts
+  end
+  
+  task :setup_ssh_keys do
+    puts deprec2_isnt_backwards_compatible  
+    puts "  Replace 'cap setup_ssh_keys' with 'cap deprec:ssh:setup_keys'"
+    puts
+  end
+  
+  task :install_rails_stack do
+    puts deprec2_isnt_backwards_compatible  
+    puts "  Replace 'cap install_rails_stack' with 'cap deprec:rails:install_rails_stack'"
+    puts
+  end
+  
+  task :setup do
+    puts deprec2_isnt_backwards_compatible  
+    puts "  Replace 'cap setup' with 'cap deploy:setup'"
+    puts
+  end
+  
+  task :restart_apache do
+    puts deprec2_isnt_backwards_compatible  
+    puts "  Replace 'cap restart_apache' with 'cap deprec:apache:restart'"
+    puts
+  end
+  
+  task :show_tasks do
+    puts deprec2_isnt_backwards_compatible  
+    puts "  Replace 'cap show-tasks' with 'cap -T'"
+    puts
   end
   
 end
