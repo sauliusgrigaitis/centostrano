48a2574610e54128f32370ace155428051b41418
mbailey <mbailey@479ab18a-db23-0410-b897-fa3724b950b0>
2008-02-18 09:00:14 +0000

    cap deprec:db:schema_load
    git-svn-id: svn://scm.deprecated.org/deprec/trunk@407 479ab18a-db23-0410-b897-fa3724b950b0

--- lib/deprec/recipes/rails.rb
+++ lib/deprec/recipes/rails.rb
@@ -286,6 +286,11 @@ Capistrano::Configuration.instance(:must_exist).load do
       task :migrate, :roles => :db do
         run "cd #{deploy_to}/current && rake db:migrate RAILS_ENV=#{rails_env}"
       end
+      
+      desc "Run database migrations"
+      task :schema_load, :roles => :db do
+        run "cd #{deploy_to}/current && rake db:schema:load RAILS_ENV=#{rails_env}"
+      end
 
       desc "Roll database back to previous migration"
       task :rollback, :roles => :db do
