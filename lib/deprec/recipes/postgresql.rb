865deda68ba215c2f025fd8d9ea73c416722fab9
saulius <saulius@ibm.(none)>
2008-04-09 22:12:28 +0300

    added postgresql support

--- lib/deprec/recipes/postgresql.rb
+++ lib/deprec/recipes/postgresql.rb
@@ -4,11 +4,9 @@ Capistrano::Configuration.instance(:must_exist).load do
     namespace :postgresql do
       
       # Installation
-      
       desc "Install postgresql"
       task :install, :roles => :db do
         install_deps
-        #symlink_mysql_sockfile # XXX still needed?
       end
       
       # Install dependencies for PostgreSQL 
@@ -16,34 +14,46 @@ Capistrano::Configuration.instance(:must_exist).load do
         apt.install( {:base => %w(postgresql postgresql-server postgresql-devel)}, :stable, :repositories => [:centosplus] )
         gem2.install "ruby-pg"
       end
-      
-      task :symlink_mysql_sockfile, :roles => :db do
+
+      desc "Create Database" 
+      task :create_db, :roles => :db do
+        #that's hack to 'generate' proper configuration file (this should be replaced with initdb or so)
+        top.centos.postgresql.start
+        top.centos.postgresql.config_gen
+        top.centos.postgresql.config
+        top.centos.postgresql.restart
+        read_config
+        createuser(db_user, db_password)
+        createdb(db_name, db_user)
+      end
+       
+      #task :symlink_mysql_sockfile, :roles => :db do
         # rails puts "socket: /tmp/mysql.sock" into config/database.yml
         # this is not the location for our ubuntu's mysql socket file
         # so we create this link to make deployment using rails defaults simpler
-        sudo "ln -sf /var/run/mysqld/mysqld.sock /tmp/mysql.sock"
-      end
+      #  sudo "ln -sf /var/run/mysqld/mysqld.sock /tmp/mysql.sock"
+      #end
       
       # Configuration
       
-      SYSTEM_CONFIG_FILES[:mysql] = [
+      SYSTEM_CONFIG_FILES[:postgresql] = [
         
-        {:template => "my.cnf.erb",
-         :path => '/etc/mysql/my.cnf',
+        {:template => "pg_hba.conf.erb",
+         :path => '/var/lib/pgsql/data/pg_hba.conf',
          :mode => 0644,
          :owner => 'root:root'}
       ]
       
-      desc "Generate configuration file(s) for mysql from template(s)"
+      desc "Generate configuration file(s) for postgresql from template(s)"
       task :config_gen do
-        SYSTEM_CONFIG_FILES[:mysql].each do |file|
-          deprec2.render_template(:mysql, file)
+        SYSTEM_CONFIG_FILES[:postgresql].each do |file|
+          deprec2.render_template(:postgresql, file)
         end
       end
       
-      desc "Push trac config files to server"
+      desc "Push postgresql config files to server"
       task :config, :roles => :db do
-        deprec2.push_configs(:mysql, SYSTEM_CONFIG_FILES[:mysql])
+        deprec2.push_configs(:postgresql, SYSTEM_CONFIG_FILES[:postgresql])
       end
       
       task :activate, :roles => :db do
@@ -75,8 +85,8 @@ Capistrano::Configuration.instance(:must_exist).load do
       task :reload, :roles => :db do
         send(run_method, "/etc/init.d/postgresql reload")
       end
-      
-      
+     
+            
       task :backup, :roles => :db do
       end
       
@@ -85,32 +95,37 @@ Capistrano::Configuration.instance(:must_exist).load do
             
     end
   end
+
+  # Imported from Rails Machine gem
+
+  def createdb(db, user)
+    sudo "su - postgres -c \'createdb -O #{user} #{db}\'"  
+  end
+  
+  def createuser(user, password)
+    cmd = "su - postgres -c \'createuser -P -D -A -E #{user}\'"
+    sudo cmd do |channel, stream, data|
+      if data =~ /^Enter password for new/
+        channel.send_data "#{password}\n" 
+      end
+      if data =~ /^Enter it again:/
+        channel.send_data "#{password}\n" 
+      end
+      if data =~ /^Shall the new role be allowed to create more new roles?/
+        channel.send_data "n\n" 
+      end
+    end
+  end
+  
+  def command(sql, database)
+    run "psql --command=\"#{sql}\" #{database}" 
+  end
+
+  def read_config
+    db_config = YAML.load_file('config/database.yml')
+    set :db_user, db_config[rails_env]["username"]
+    set :db_password, db_config[rails_env]["password"] 
+    set :db_name, db_config[rails_env]["database"]
+  end
+
 end
-
-#
-# Setup replication
-#
-
-# setup user for repl
-# GRANT REPLICATION SLAVE ON *.* TO 'repl'@'%.yourdomain.com' IDENTIFIED BY 'slavepass';
-
-# get current position of binlog
-# mysql> FLUSH TABLES WITH READ LOCK;
-# Query OK, 0 rows affected (0.00 sec)
-# 
-# mysql> SHOW MASTER STATUS;
-# +------------------+----------+--------------+------------------+
-# | File             | Position | Binlog_Do_DB | Binlog_Ignore_DB |
-# +------------------+----------+--------------+------------------+
-# | mysql-bin.000012 |      296 |              |                  | 
-# +------------------+----------+--------------+------------------+
-# 1 row in set (0.00 sec)
-# 
-# # get current data
-# mysqldump --all-databases --master-data >dbdump.db
-# 
-# UNLOCK TABLES;
-
-
-# Replication Features and Issues
-# http://dev.mysql.com/doc/refman/5.0/en/replication-features.html
