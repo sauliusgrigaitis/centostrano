19b135b90889b5c7b21b152549a4bd048621024b
saulius <saulius.grigaitis@mif.vu.lt>
2008-05-28 11:40:09 +0300

    added mising monit instalation

--- lib/deprec/recipes/monit.rb
+++ lib/deprec/recipes/monit.rb
@@ -43,6 +43,7 @@ Capistrano::Configuration.instance(:must_exist).load do
   task :install do
     install_deps
     enable_rmpforge
+    apt.install( {:base => %w(monit)}, :stable )
     #deprec2.download_src(SRC_PACKAGES[:monit], src_dir)
     #deprec2.install_from_src(SRC_PACKAGES[:monit], src_dir)
     activate
