9013922cf9ab787fb1f525601f7f863c737461d3
saulius <saulius.grigaitis@mif.vu.lt>
2008-07-01 15:22:48 +0300

    fixed logrotate namespace

--- lib/deprec/recipes/logrotate.rb
+++ lib/deprec/recipes/logrotate.rb
@@ -1,6 +1,6 @@
 # Copyright 2006-2008 by Mike Bailey. All rights reserved.
 Capistrano::Configuration.instance(:must_exist).load do 
-  namespace :deprec do 
+  namespace :centos do 
     namespace :logrotate do
 
       # Install      
