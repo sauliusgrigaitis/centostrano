f1ebc80b983341ed6997e53aa3241a7d492bae18
Saulius Grigaitis <saulius.grigaitis@mif.vu.lt>
2009-07-02 20:51:05 +0300

    Updated from deprec2: Removed hard coded variables 9adab0e1f2292a85e4a49cc3d5bbf49eb26c9bcc [Mike Bailey]

--- lib/deprec/recipes/mysql.rb
+++ lib/deprec/recipes/mysql.rb
@@ -118,10 +118,6 @@ Capistrano::Configuration.instance(:must_exist).load do
       
       desc "Grant user access to database" 
       task :grant_user_access_to_database, :roles => :db do
-        set :db_user, 'mbailey'
-        set :db_name, 'testy'
-        set :db_password, 'as'
-        
         cmd = "GRANT ALL PRIVILEGES ON #{db_name}.* TO '#{db_user}'@localhost IDENTIFIED BY '#{db_password}';"
         run "mysql -u #{mysql_admin_user} -p #{db_name} -e \"#{cmd}\"" do |channel, stream, data|
           if data =~ /^Enter password:/
